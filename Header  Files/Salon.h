6c57b90ffc1a44bbaf907ac159eaddb9884a149b
Grzegorz Kotkowski <149706238+lunnarphase@users.noreply.github.com>
2025-01-04 13:27:45 +0100

    Utworzenie klasy Salon

--- Header  Files/Salon.h
+++ Header  Files/Salon.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include <queue>
+#include <mutex>
+
+using namespace std;
+
+class Salon {
+public:
+	int wolneFotele;
+	int pojemnoscPoczekalni;
+
+	queue<int> kolejkaKlientow; // kolejka klientow w poczekalni
+
+	mutex mtxPoczekalnia; // muteks do synchronizacji poczekalni
+	mutex mtxFotele; // muteks do synchronizacji dostepu do foteli
+
+	condition_variable cvPoczekalnia; // zmienna warunkowa dla fryzjerow czekajacych na klientow
+
+	Salon(int nFotele, int kPoczekalnia);
+};
--- Source Files/Salon.cpp
+++ Source Files/Salon.cpp
@@ -1,1 +1,4 @@
 #include "Salon.h"
+
+Salon::Salon(int nFotele, int kPoczekalnia)
+	: wolneFotele(nFotele), pojemnoscPoczekalni(kPoczekalnia) { }
